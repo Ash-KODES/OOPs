f6a61593fce207f3324618c3c9f2ff9629ebb184
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-03-18 23:41:36 +0530

    1.classes basic

---
+++ 1.Class.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Human{
+    int leg;
+    int hand;
+};
+int main()
+{
+    Human ashu;
+    //ashu is a object of type Human
+    ashu.leg=2;
+    ashu.hand=2;
+    return 0;
+}
